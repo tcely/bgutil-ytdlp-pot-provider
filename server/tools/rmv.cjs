b3a9cebbc2d472abfb5307dee40279822a620c53
tcely <tcely@users.noreply.github.com>
2026-01-28 06:52:28 -0500

    Update rmv.cjs

--- server/tools/rmv.cjs
+++ server/tools/rmv.cjs
@@ -8,12 +8,14 @@ const rawArgs = process.argv.slice(2);
 const dashIndex = rawArgs.indexOf("--");
 
 // Arguments before -- are flags; arguments after are always targets
-const flagArgs = -1 !== dashIndex
-    ? rawArgs.slice(0, dashIndex)
-    : rawArgs.filter((a) => a.startsWith("-"));
-const targetArgs = -1 !== dashIndex
-    ? rawArgs.slice(1 + dashIndex)
-    : rawArgs.filter((a) => !a.startsWith("-"));
+const flagArgs =
+    -1 !== dashIndex
+        ? rawArgs.slice(0, dashIndex)
+        : rawArgs.filter((a) => a.startsWith("-"));
+const targetArgs =
+    -1 !== dashIndex
+        ? rawArgs.slice(1 + dashIndex)
+        : rawArgs.filter((a) => !a.startsWith("-"));
 
 const isRecursive = flagArgs.some((f) => f.includes("r"));
 const isForce = flagArgs.some((f) => f.includes("f"));
@@ -27,21 +29,21 @@ if (targetArgs.length === 0) {
 
 function rmv(p, topLevel = true) {
     if (!fs.existsSync(p)) {
-      if (!isForce && topLevel) {
-        console.error(`rmv: cannot remove '${p}': No such file or directory`);
-        process.exit(1);
-      }
-      return;
+        if (!isForce && topLevel) {
+            console.error(`rmv: cannot remove '${p}': No such file or directory`);
+            process.exit(1);
+        }
+        return;
     }
 
     const stats = fs.lstatSync(p);
     if (stats.isDirectory()) {
-      if (!isRecursive) {
-        console.error(`rmv: cannot remove '${p}': Is a directory`);
-        process.exit(1);
-      }
-      // Walk tree for verbose logging matching 'rm -v'
-      fs.readdirSync(p).forEach((file) => rmv(path.join(p, file), false));
+        if (!isRecursive) {
+            console.error(`rmv: cannot remove '${p}': Is a directory`);
+            process.exit(1);
+        }
+        // Walk tree for verbose logging matching 'rm -v'
+        fs.readdirSync(p).forEach((file) => rmv(path.join(p, file), false));
     }
 
     // Final removal and verbose log:
